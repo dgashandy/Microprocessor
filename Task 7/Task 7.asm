da449d38d3c33ead715a80b15fecbad7fe0d7a34
Piggy Assault <99483955+dgashandy@users.noreply.github.com>
2022-08-29 00:23:33 +0700

    Update Task 7.asm

--- Task 7/Task 7.asm
+++ Task 7/Task 7.asm
@@ -1,48 +1,74 @@
-extern GetStdHandle
-extern WriteFile
-extern ReadFile
-extern AllocConsole
-extern FreeConsole
-extern SetConsoleTitleA
-extern Sleep
-extern ExitProcess
-import GetStdHandle kernel32.dll
-import WriteFile kernel32.dll
-import ReadFile kernel32.dll
-import AllocConsole kernel32.dll
-import FreeConsole kernel32.dll
-import SetConsoleTitleA kernel32.dll
-import Sleep kernel32.dll
-import ExitProcess kernel32.dll
-%macro initconsole 3
-CALL [AllocConsole]
-PUSH dword %1
-CALL [SetConsoleTitleA]
-PUSH dword -10
-CALL [GetStdHandle]
-MOV dword [%2], EAX
-PUSH dword -11
-CALL [GetStdHandle]
-MOV dword [%3], EAX
-%endmacro
-%macro quitconsole 0
-CALL [FreeConsole]
-XOR EAX,EAX
-PUSH EAX
-CAll [ExitProcess]
-%endmacro%macro TampilkanTeks 4
+%INCLUDE 'Console.API'
+%INCLUDE 'Console.MAC'
+SEGMENT .DATA USE32
+Title db "Penjumlahan",0
+title1 db "Total Data",0
+Msg1 db "Tulis data-data, akhiri dengan 0",13,10,13,10,0
+Msg1_len dd $-Msg1
+Buff times 255 db 13
+Buff_len dd $-Buff
+PJ db 'Total data: '
+totalhasil db ' ',13,10
+db 'Lagi? '
+db 13,10,0
+SEGMENT .BSS USE32
+hStdOut resd 1
+hStdIn resd 1
+nBytes resd 1
+iBytes resd 1
+jumlah resd 1
+SEGMENT .CODE USE32
+..start:BuatConsole Title, hStdIn, hStdOut
+ulangi:
+TampilkanTeks hStdOut, Msg1, Msg1_len, nBytes
+BacaTeks hStdIn, Buff, Buff_len, iBytes
+CALL str2num
+mov [jumlah],eax
+Inputloop:
+BacaTeks hStdIn, Buff, Buff_len, iBytes
+CALL str2num
+add [jumlah],eax
+cmp eax,0
+jnz Inputloop
+mov eax,[jumlah]
+mov ebx, totalhasil
+loop1:
+cmp byte[ebx],13
+je exit1
+mov byte[ebx],' '
+inc ebx
+jmp loop1
+exit1:
+dec ebx
+mov si,10
+loop2:
+sub edx, edx
+div si
+add dl, '0'
+mov [ebx],dl
+dec ebx
+or eax,eax
+jnz loop2
+push dword (4+64)
+push dword title1
+push dword PJ
 push dword 0
-push dword %4
-push dword [%3]
-push dword %2
-push dword [%1]
-call [WriteFile]
-%endmacro
-%macro BacaTeks 4
-push dword 0
-push dword %4
-push dword [%3]
-push dword %2
-push dword [%1]
-call [ReadFile]
-%endmacro
+call[MessageBoxA]
+CMP EAX, 6
+JE ulangiTutupConsole 10
+RET
+str2num:
+xor eax,eax
+mov esi,10
+mov ebx,Buff
+mov ecx, [iBytes]
+sub ecx, 2
+xor edx,edx
+loopbil:
+mul esi
+mov dl, byte[ebx]
+sub dl,30h
+add eax,edx
+inc ebx
+loop loopbil
+RET
